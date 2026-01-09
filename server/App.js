b1c2ff5f5a25f26ea9f82ffa61f44ca2ceee8b86
Amran <amraanhossen@gmail.com>
2026-01-10 00:11:39 +0600

    edit

--- server/App.js
+++ server/App.js
@@ -1,26 +1,36 @@
 import express from "express";
-import connectDB from './db.js';
-import UserRouter from './Router/user.router.js';
+import connectDB from "./db.js";
+import UserRouter from "./Router/user.router.js";
+import cors from "cors";
 
-import cors from 'cors';
 const app = express();
-app.use(cors())
-connectDB()
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-// user router
-app.use("/user",UserRouter);
-//info route 
 
-//testing
-app.get("/",(req,res)=>{
-  res.send("Hi my I am Amran")
-})
-//error route 
-app.use((req,res,next)=>{
-  console.log("404 route error")
-})
-app.use((err,req,res,next)=>{
-  console.log("500 server error");
-})
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// database connect
+connectDB()
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("DB connection error:", err));
+
+// routes
+app.use("/user", UserRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am Amran");
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// 500 error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Server error", message: err.message });
+});
+
 export default app;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import app from './App.js';
 import dotenv from 'dotenv';
-dotenv.config();
-const port = process.env.PORT;
 
-app.listen(port,()=>{
-  console.log(`Your server is running at ${port}`)
-})
-export default app;
+dotenv.config();
+
+const PORT = process.env.PORT || 4000; // fallback if PORT not set
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
+});
